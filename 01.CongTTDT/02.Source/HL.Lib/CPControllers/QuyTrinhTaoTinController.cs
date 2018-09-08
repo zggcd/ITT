4b8fed29cc3518471af60db53010bd802dc79017
Tung Lam <41668405+tunglam009@users.noreply.github.com>
2018-09-08 13:39:40 +0700

    ITT Update

---
+++ 01.CongTTDT/02.Source/HL.Lib/CPControllers/QuyTrinhTaoTinController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+using HL.Lib.MVC;
+using HL.Lib.Models;
+using HL.Lib.Global;
+
+namespace HL.Lib.CPControllers
+{
+    public class QuyTrinhTaoTinController : CPController
+    {
+        public QuyTrinhTaoTinController()
+        {
+            CheckPermissions = false;
+        }
+
+        public void ActionIndex(QuyTrinhTaoTinModel model)
+        {
+            ViewBag.Model = model;
+        }
+    }
+
+    public class QuyTrinhTaoTinModel : DefaultModel
+    {
+        private int _LangID = 1;
+        public int LangID
+        {
+            get { return _LangID; }
+            set { _LangID = value; }
+        }
+
+        public int MenuID { get; set; }
+        public int State { get; set; }
+        public string SearchText { get; set; }
+
+        public int[] ArrState { get; set; }
+    }
+}
